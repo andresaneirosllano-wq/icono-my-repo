5265b2d7941e9d91ac573b19c31a5021247281f1
aaneiros <aaneiros@indra.es>
2026-04-14 11:50:07 +0200

    Cambiosconflictyos

--- main.js
+++ main.js
@@ -6,4 +6,4 @@ console.log("Hi 4_desde rama SegundaRama");
 console.log("Hi 4_desde rama SegundaRama. Segundo cambio");
 console.log("Hi 4_desde rama TerceraRama");
 console.log("Hi 4_desde rama master");
-console.log("Hi 4_desde Xarta rama");
+console.log("Hi 4_desde Xarta rama22");
